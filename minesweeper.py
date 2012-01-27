0d366529b9ee61746684b73fefc847fb8afca1c5
Alessandro Morgantini <gpz500@technologist.com>
2012-01-27 14:46:56 +0000

    Modulo Python per implementare un gioco stile Minesweeper di Windows.

---
+++ minesweeper.py
@@ -0,0 +1,394 @@
+"""Minesweeper game.
+
+This module implements two classes to implement a minesweeper game.
+The classes are:
+    - Cell, for a single cell
+    - Game, for a two dimensional array of cells
+    
+If game = Game(), the cells are addressed as game[i][j] where 0 <= i <= nrows
+and 0 <= j <= ncols.
+"""
+
+
+__author__ = "Alessandro Morgantini <gpz500@technologist.com>"
+__version__ = "$Revision$"
+__date__ = "$Date$"
+__copyright__ = "Copyright (c) 2012 Alessandro Morgantini"
+__license__ = "Python"
+
+
+class MinesweeperError( Exception ):
+    pass
+    
+class MinesweeperStatusError( MinesweeperError ):
+    pass
+    
+class MinesweeperAutoUncoverError( MinesweeperError ):
+    pass
+    
+class MinesweeperCellError( MinesweeperError ):
+    pass
+
+class MinesweeperMinesCount( MinesweeperError ):
+    pass
+
+class Cell:
+    """This is a class for a single cell.
+    
+    Create an instance of this class for every cell in the map when a game is
+    started."""
+    
+    # Some usesul constants
+    COVERED = 0
+    REVEALED = 1
+    FLAG = 2
+    Q_MARK = 3
+    
+    def __init__( self, x, y ):
+        """The init method for a instance.
+        The arguments are:
+    
+        x, y:     the coordinates of the cell
+        mine:     is there a mine (True or False)?"""
+        if x < 0 or y < 0:
+            raise MinesweeperCellError, "Coordinates must be both >= 0"
+    
+        self.x = x
+        self.y = y
+            
+        # This is the cell status (initially COVERED)
+        self.status = self.COVERED
+        
+        # This is the number of neighbor cells with a mine (initially 0)
+        self.neighborMines = 0
+        
+        # This is the presence of a mine or not (initially false)
+        self.mine = False
+        
+    def SetNeighbors( self, neighbors ):
+        """Set the actual number of neighbors."""
+        self.neighbors = neighbors
+        
+    def GetNeighborMinesNum( self ):
+        """Return the actual number of neighbors."""
+        return self.neighborMines
+        
+    def IncNeighbors( self ):
+        """Increment the numbers of neighbors mines by 1."""
+        self.neighborMines += 1
+        
+    def GetCoordinates( self ):
+        """Return a tuple with two elements: (x, y)."""
+        return ( self.x, self.y )
+        
+    def HasMine( self ):
+        """Return if the cell has a mine or not."""
+        return self.mine
+        
+    def SetMine( self, reset = False ):
+        """Set mine as true."""
+        self.mine = not reset
+        
+    def GetStatus( self ):
+        """Return the current status of cell."""
+        return self.status
+        
+    def SetStatus( self, newstatus ):
+        """Set current status of the cell."""
+        if newstatus == self.status:
+            raise MinesweeperStatusError, "Error: can't reassign the same status"
+        if self.status == Cell.FLAG and newstatus == Cell.REVEALED:
+            raise MinesweeperStatusError, "Error: can't do the transition FLAG -> REVEALED"
+        if self.status == Cell.REVEALED:
+            raise MinesweeperStatusError, "Error: can't come back from REVEALED status"
+        self.status = newstatus
+
+def GetRandomPos( nrows, ncols ):
+    """Static method to compute random pos between 0 - (nrows-1) and 0 - (ncols-1)."""
+    import random
+    i = random.randint( 0, nrows - 1 )
+    j = random.randint( 0, ncols - 1 )
+    return ( i, j )
+        
+class Game( list ):
+    """A class for a whole minesweeper game."""
+    
+    def __init__( self, nrows = 16, ncols = 30, nmines = 99 ):
+        """Initialize a game with nrows, ncols and nmines set randomnly on the table."""
+        
+        # Check for the acceptable mines number
+        if nmines > nrows * ncols:
+            raise MinesweeperMinesCount, "Too much mines!"
+        
+        # I have to create nrows list ncols cells
+        self.cells = []
+        
+        # The number of cells without bombs: every time you discover a cell this
+        # count cuts
+        self.toDiscover = nrows * ncols - nmines
+        
+        # Create all the cells
+        for i in range( nrows ):
+            row = []
+            for j in range( ncols ):
+                row.append( Cell( i, j ) )
+                
+            self.cells.append( row )
+        
+        # Now I have to put nmines randomly in the cells
+        mines = []
+        while len( mines ) < nmines:
+            # Computes a random position for the mine. If it already exists, compute a new one
+            i, j = GetRandomPos( nrows, ncols )
+            while ( i, j ) in mines:
+                i, j = GetRandomPos( nrows, ncols )
+            mines.append( (i, j) )
+            self[ i ][ j ].SetMine()
+            
+        
+        # Now I have a list of mines. For every mine I need to
+        # increment the number of neighbors in the neighbors cells
+        for i, j in mines:
+            for cell in self.GetNeighborsList( i, j ):
+                cell.IncNeighbors()
+        
+
+
+    def __getitem__( self, index ):
+        """Game is a subclass of list, so it returns the index-th rows when asked."""
+        return self.cells[ index ]
+        
+    
+    def __len__( self ):
+        """Return the number of rows."""
+        return len( self.cells )
+        
+    def __iter__( self ):
+        """Return an iterator object specifically for 'for'."""
+        return self.cells.__iter__()
+        
+    def GetNeighborsList( self, i, j = -1 ):
+        """Compute a list of neighbors."""
+        li = []
+        if j == -1:
+            # i is the cell object
+            j = i.y
+            i = i.x
+                
+        imin = max( i - 1, 0 )
+        imax = min( i + 1, len( self ) - 1 )
+        jmin = max( j - 1, 0 )
+        jmax = min( j + 1, len( self[ 0 ] ) - 1 )
+        for ii in range( imin, imax + 1 ):
+            for jj in range( jmin, jmax + 1 ):
+                if ( ii, jj ) != ( i, j ): li.append( self[ ii ][ jj ] ) 
+                
+        return li
+        
+    def Uncover( self, i, j ):
+        """Uncover the cell (i, j). Return True if there is a mine, False otherwise."""
+        self[ i ][ j ].SetStatus( Cell.REVEALED )
+        self.toDiscover -= 1
+        if self[ i ][ j ].HasMine():
+            return True
+            
+        # Undiscover the neighbords also, but only if this cell have non close mines
+        if not self[ i ][ j ].GetNeighborMinesNum():
+            self.AutomaticUncover( i, j )
+                
+        return False
+        
+    def AutomaticUncover( self, i, j ):
+        """Uncover a chain of cells by neighboroad relation."""
+        for cell in self.GetAutoUncoverList( self[ i ][ j ] ):
+            cell.SetStatus( Cell.REVEALED )
+            self.toDiscover -= 1
+            
+    def GetAutoUncoverList( self, cell ):
+        """Return the list of cells to uncover automatically, starting from the supplied cell."""
+        if cell.GetNeighborMinesNum():
+            raise MinesweeperAutoUncoverError, "Error: can't automatic uncovers cells with some close mine!"
+        
+        myFiltFunction = lambda x: x.GetStatus() != Cell.REVEALED
+        toUncover = filter( myFiltFunction, self.GetNeighborsList( cell ) )
+        for nei in toUncover:
+            if not nei.GetNeighborMinesNum():
+                neiToUncover = filter( myFiltFunction, [ item for item in self.GetNeighborsList( nei ) if not item in toUncover ] )
+                toUncover.extend( neiToUncover )
+                
+        return toUncover
+        
+            
+        
+    def Flag( self, i, j, reset = False ):
+        """Set/Reset a flag."""
+        newstatus = reset and Cell.COVERED or Cell.FLAG
+        self[ i ][ j ].SetStatus( newstatus )
+        
+    def QMark( self, i, j, reset = False ):
+        """Set/Reset a flag."""
+        newstatus = reset and Cell.COVERED or Cell.Q_MARK
+        self[ i ][ j ].SetStatus( newstatus )
+        
+    def GetToDiscover( self ):
+        """Return the number of cells remaining to discover."""
+        return self.toDiscover
+        
+    def SetMines( self, minesList ):
+        """Set a known minelist. minesList is a list of coordinates."""
+        for row in self:
+            for cell in row:
+                cell.SetMine( not cell.GetCoordinates() in minesList )
+                # Reset the number of neighbors
+                cell.SetNeighbors( 0 )
+                
+        # I have a list of mines. For every mine I need to
+        # increment the number of neighbors in the neighbors cells
+        for i, j in minesList:
+            for cell in self.GetNeighborsList( i, j ):
+                cell.IncNeighbors()
+                
+    def GetMines( self ):
+        """Return a list of coordinates of current mines."""
+        mines = []
+        for row in self:
+            for cell in row:
+                if cell.HasMine():
+                    mines.append( cell.GetCoordinates() )            
+        return mines
+        
+    def Restart( self ):
+        """Reinit the game with the same mine list."""
+        mines = self.GetMines()
+        nrows = len( self )
+        ncols = len( self[ 0 ] )
+        
+        self.cells = []
+        while len( self.cells ) < nrows:
+            i = len( self.cells )
+            row = []
+            while len( row ) < ncols:
+                j = len( row )
+                row.append( Cell( i, j ) )
+            self.cells.append( row )
+            
+        self.SetMines( mines )
+        self.toDiscover = nrows * ncols - len( mines )
+            
+    
+def PrintGame( game, unveil = False ):
+    """Print the table of games, with actual covered, flagged, q_mark."""
+    nrows = len( game )
+    ncols = len( game[ 0 ] )
+    lines = []
+    
+    # First & second line
+    tents = [ ' ' ]     # First column for the ABC...
+    units = [ ' ' ]
+    for j in range( 1, ncols + 1 ):
+        quot = j / 10
+        remain = j % 10
+        if quot:
+            tents.append( str( quot ) )
+        else:
+            tents.append( ' ' )
+        units.append( str( remain ) )
+    lines.append( "".join( tents ) )
+    lines.append( "".join( units ) )
+    
+    # The table
+    for i in range( nrows ):
+        line = [ chr( ord('A') + i ) ]
+        for j in range( ncols ):
+            status = game[ i ][ j ].GetStatus()
+            neigh = game[ i ][ j ].GetNeighborMinesNum()
+            
+            if unveil:
+                if game[ i ][ j ].HasMine():    line.append( '*' )
+                # From here in on the cell does not have a mine
+                elif status == Cell.FLAG:       line.append( 'E' )  # Error
+                elif status == Cell.Q_MARK:     line.append( '?' )
+                elif status == Cell.COVERED:    line.append( '-' )
+                else:
+                    # Uncovered
+                    if neigh == 0:              line.append( ' ' )
+                    else:                       line.append( str( neigh ) )
+            else:
+                if status == Cell.FLAG:         line.append( '*' )
+                elif status == Cell.Q_MARK:     line.append( '?' )
+                elif status == Cell.COVERED:    line.append( '-' )
+                else:
+                    # Uncovered
+                    if neigh == 0:              line.append( ' ' )
+                    else:                       line.append( str( neigh ) )
+        
+        
+        lines.append( "".join( line ) )
+    
+
+
+    print "\n".join( lines )
+            
+        
+    
+    
+if __name__ == '__main__':
+    # Do a game on terminal
+    import sys
+    import re
+    myGame = Game( 9, 9, 10 )
+    
+    while True:
+        PrintGame( myGame )
+        print "[Q|B i j|? i j|R i j] > ",
+        command = sys.stdin.readline()
+        print
+        
+        myResult = re.search( '\s?[qQ]\s', command )
+        if myResult:
+            # Q: quit
+            exit()
+            
+        myResult = re.search( '\s?([bB\?rR])\s([A-Za-z])\s(\d+)\s', command )
+        if myResult:
+            command = myResult.group( 1 ).upper()
+            i = ord( myResult.group( 2 ).upper() ) - ord( 'A' )
+            j = int( myResult.group( 3 ) ) - 1
+            
+            # Check if i, j are OK
+            if ( not 0 <= i < len( myGame ) ) or ( not 0 <= j < len( myGame[ 0 ] )):
+                print "Coordinates are out of range!"
+                continue
+                
+            if command == 'B':
+                # Sign a bomb
+                if myGame[ i ][ j ].GetStatus() == Cell.FLAG:
+                    myGame.Flag( i, j, True )
+                else:
+                    myGame.Flag( i, j )
+            elif command == '?':
+                # Question mark on a cell
+                if myGame[ i ][ j ].GetStatus() == Cell.Q_MARK:
+                    myGame.QMark( i, j, True )
+                else:
+                    myGame.QMark( i, j )
+            elif command == 'R':
+                # Reveal a cell
+                try:
+                    mine = myGame.Uncover( i, j )
+                except MinesweeperStatusError:
+                    mine = False
+                if mine:
+                    print "Bomb!"
+                    PrintGame( myGame, True )
+                    print "Do you want to replay? [y|N]> "
+                    command = sys.stdin.readline()
+                    if command.lstrip().upper()[ 0 ] != 'Y':
+                        exit()
+                    else:
+                        myGame.Restart()
+                elif myGame.GetToDiscover() == 0:
+                    print "You won!"
+                    PrintGame( myGame )
+                    exit()
+            
---
+++ minesweepertest.py
@@ -0,0 +1,263 @@
+"""Unit test for module minesweeper.py.
+
+minesweeper define two classes: Cell for a single cell and Game for a whole table.
+"""
+
+__author__ = "Alessandro Morgantini <gpz500@technologist.com>"
+__version__ = "$Revision$"
+__date__ = "$Date$"
+__copyright__ = "Copyright (c) 2012 Alessandro Morgantini"
+__license__ = "Python"
+
+import unittest
+import minesweeper
+
+knownCoordinates = ( ( 0, 25 ),
+                     ( 34, 5 ),
+                     ( 12, 99 ),
+                     ( 1, 3 ),
+                     ( 3, 1 ),
+                     ( 13, 10 ),
+                     ( 0, 0 ),
+                     ( 4, 4 ) )
+
+
+class CellTest( unittest.TestCase ):
+    
+    statuses = ( minesweeper.Cell.COVERED,
+                 minesweeper.Cell.FLAG,
+                 minesweeper.Cell.Q_MARK,
+                 minesweeper.Cell.REVEALED )
+
+    def testSetStatus( self ):
+        """Cell instances have to set properly its status."""
+        cell = minesweeper.Cell( 10, 11 )
+        cell.SetStatus( -1 )
+        for status in self.statuses:
+            cell.SetStatus( status )
+            self.assertEqual( status, cell.GetStatus() )
+            
+    def testRetainCoordinates( self ):
+        """Cell instances have to retain correctly their coordinates."""
+        for x, y in knownCoordinates:
+            cell = minesweeper.Cell( x, y )
+            self.assertEqual( (x, y), cell.GetCoordinates() )
+            
+    def testCoordinatesNotNegative( self ):
+        """Cell.__init__ must raise an exception when a coordinate is negative."""
+        self.assertRaises( minesweeper.MinesweeperCellError, minesweeper.Cell, -1, 1 )
+        self.assertRaises( minesweeper.MinesweeperCellError, minesweeper.Cell, 1, -1 )
+        
+    def testInvalidSetStatus( self ):
+        """Cell instances have to raise exception in invalid status transition."""
+        cell = minesweeper.Cell( 0, 0 )
+        cell.SetStatus( minesweeper.Cell.REVEALED )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.COVERED )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.REVEALED )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.FLAG )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.Q_MARK )
+        cell = minesweeper.Cell( 0, 0 )
+        cell.SetStatus( minesweeper.Cell.FLAG )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.REVEALED )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.FLAG )
+        cell = minesweeper.Cell( 0, 0 )
+        cell.SetStatus( minesweeper.Cell.Q_MARK )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.Q_MARK )
+        cell.SetStatus( minesweeper.Cell.REVEALED )
+        self.assertRaises( minesweeper.MinesweeperStatusError, cell.SetStatus, minesweeper.Cell.REVEALED )
+        
+    def testIncNeighbors( self ):
+        """Cell must properly increment the number o neighbors."""
+        cell = minesweeper.Cell( 0, 0 )
+        for n in range( 100 ):
+            cell.SetNeighbors( n )
+            cell.IncNeighbors()
+            self.assertEqual( n + 1, cell.GetNeighborMinesNum() )
+            
+    def testMines( self ):
+        """Cell must set & get mines properly."""
+        cell = minesweeper.Cell( 0, 0 )
+        self.assertEqual( False, cell.HasMine() )
+        cell.SetMine()
+        self.assertEqual( True, cell.HasMine() )
+        cell.SetMine( reset = True )
+        self.assertEqual( False, cell.HasMine() )
+        
+class GameTest( unittest.TestCase ):
+
+    knownNeighboards = {
+        ( 0, 0 ):   ( (0, 1), (1, 0), (1, 1) ),
+        ( 15, 29 ): ( (14, 28), (14, 29), (15, 28) ),
+        ( 0, 29 ):  ( (0, 28), (1, 28), (1, 29) ),
+        ( 15, 0 ):  ( (14, 0), (14, 1), (15, 1) ),
+        ( 0, 10 ):  ( (0, 9), (0, 11), (1, 9), (1, 10), (1, 11) ),
+        ( 10, 0 ):  ( (9, 0), (9, 1), (10, 1), (11, 0), (11, 1) ),
+        ( 10, 29 ): ( (9, 28), (9, 29), (10, 28), (11, 28), (11, 29) ),
+        ( 15, 10 ): ( (14, 9), (14, 10), (14, 11), (15, 9), (15, 11) ),
+        ( 3, 4 ):   ( (2, 3), (2, 4), (2, 5), (3, 3), (3, 5), (4, 3), (4, 4), (4, 5) )
+    }
+                         
+    knownMines = ( (0, 1), (2, 1), (6, 3), (8, 5), (1, 1), (8, 3), (5, 1), (6, 6), (7, 8), (0, 7) )
+    knownNeighMines = {
+         ( 0, 0 ): 2,
+         ( 0, 2 ): 2,
+         ( 0, 6 ): 1,
+         ( 0, 8 ): 1,
+         ( 1, 0 ): 3,
+         ( 1, 2 ): 3,
+         ( 1, 6 ): 1,
+         ( 1, 7 ): 1,
+         ( 1, 8 ): 1,
+         ( 2, 0 ): 2,
+         ( 2, 2 ): 2,
+         ( 3, 0 ): 1,
+         ( 3, 1 ): 1,
+         ( 3, 2 ): 1,
+         ( 4, 0 ): 1,
+         ( 4, 1 ): 1,
+         ( 4, 2 ): 1,
+         ( 5, 0 ): 1,
+         ( 5, 2 ): 2,
+         ( 5, 3 ): 1,
+         ( 5, 4 ): 1,
+         ( 5, 5 ): 1,
+         ( 5, 6 ): 1,
+         ( 5, 7 ): 1,
+         ( 6, 0 ): 1,
+         ( 6, 1 ): 1,
+         ( 6, 2 ): 2,
+         ( 6, 3 ): 1,
+         ( 6, 4 ): 1,
+         ( 6, 5 ): 1,
+         ( 6, 7 ): 2,
+         ( 6, 8 ): 1,
+         ( 7, 2 ): 2,
+         ( 7, 3 ): 2,
+         ( 7, 4 ): 3,
+         ( 7, 5 ): 2,
+         ( 7, 6 ): 2,
+         ( 7, 7 ): 2,
+         ( 8, 2 ): 1,
+         ( 8, 4 ): 2,
+         ( 8, 6 ): 1,
+         ( 8, 7 ): 1,
+         ( 8, 8 ): 1
+         }
+    
+    # Know autouncover for (8, 0)
+    autoUncoverStart = ( 8, 0 )
+    knownAutouncover = (
+        ( 7, 0 ),
+        ( 7, 1 ),
+        ( 8, 1 ),
+        ( 6, 0 ),
+        ( 6, 1 ),
+        ( 6, 2 ),
+        ( 7, 2 ),
+        ( 8, 2 )
+    )
+
+    def testInit( self ):
+        """Game.__init__ must create the exact number of cells."""
+        game = minesweeper.Game( 9, 21, 0 )
+        self.assertEqual( 9, len( game ) )
+        self.assertEqual( 21, len( game[ 0 ] ) )
+
+    
+    def testNeighbors( self ):
+        """Game must compute the list of neighbors properly for a list of known cells."""
+        game = minesweeper.Game()
+        for ( i, j ), known in self.knownNeighboards.items():
+            computedList = [ cell.GetCoordinates() for cell in game.GetNeighborsList( i, j ) ]
+            self.assertEqual( known,  tuple( computedList ) )
+            
+    def testSetMines( self ):
+        """Game must set correctly the mine from a known list."""
+        if len( self.knownMines ) <= 25:
+            nrows = 9
+            ncols = 9
+        elif 26 <= len( self.knownMines ) <= 76:
+            nrows = 16
+            ncols = 16
+        else:
+            nrows = 16
+            ncols = 30
+        game = minesweeper.Game( nrows, ncols, 0 )
+        
+        game.SetMines( self.knownMines )
+        minesCount = 0
+        for row in game:
+            for cell in row:
+                if cell.HasMine():
+                    minesCount += 1
+                    self.assertEqual( True, cell.GetCoordinates() in self.knownMines )
+                else:
+                    self.assertEqual( False, cell.GetCoordinates() in self.knownMines )
+                    
+        self.assertEqual( minesCount, len( self.knownMines ) )
+                    
+    def testNeighMines( self ):
+        """Game must compute correctly the number of neighbors mines."""
+        if len( self.knownMines ) <= 25:
+            game = minesweeper.Game( 9, 9, 0 )
+        elif 26 <= len( self.knownMines ) <= 76:
+            game = minesweeper.Game( 16, 16, 0 )
+        else:
+            game = minesweeper.Game()
+            
+        game.SetMines( self.knownMines )
+        for row in game:
+            for cell in row:
+                if not cell.HasMine():
+                    if cell.GetCoordinates() in self.knownNeighMines.keys():
+                        self.assertEqual( self.knownNeighMines[ cell.GetCoordinates() ], cell.GetNeighborMinesNum(),
+                            "Should be %d, is %d (cell (%d, %d))" %
+                                ( self.knownNeighMines[ cell.GetCoordinates() ],
+                                  cell.GetNeighborMinesNum(),
+                                  cell.GetCoordinates()[ 0 ],
+                                  cell.GetCoordinates()[ 1 ] ) )
+                    else:
+                        self.assertEqual( 0, cell.GetNeighborMinesNum(),
+                            "Should be 0, is %d (cell (%d, %d))" %
+                                ( cell.GetNeighborMinesNum(),
+                                  cell.GetCoordinates()[ 0 ],
+                                  cell.GetCoordinates()[ 1 ] ) )
+                
+                         
+    def testAutoUncover( self ):
+        """Game must to autoreveal cells for known conditions."""
+        if len( self.knownMines ) <= 25:
+            game = minesweeper.Game( 9, 9, 0 )
+        elif 26 <= len( self.knownMines ) <= 76:
+            game = minesweeper.Game( 16, 16, 0 )
+        else:
+            game = minesweeper.Game()
+            
+        game.SetMines( self.knownMines )
+        startCell = game[ self.autoUncoverStart[ 0 ] ][ self.autoUncoverStart[ 1 ] ]
+        startCell.SetStatus( minesweeper.Cell.REVEALED )
+        coordList = [ cell.GetCoordinates() for cell in game.GetAutoUncoverList( startCell ) ]
+        self.assertEqual( tuple( coordList ), self.knownAutouncover )
+        
+    def testRestart( self ):
+        """Game have to put all the cell statuses in COVERED and reset the count of uncovered cells."""
+        game = minesweeper.Game()
+        minesList = game.GetMines()
+        
+        game.QMark( 10, 21 )
+        game.Flag( 10, 22 )
+        game.QMark( 10, 20 )
+        game.Flag( 10, 23 )
+        
+        
+        game.Restart()
+        for row in game:
+            for cell in row:
+                self.assertEqual( minesweeper.Cell.COVERED, cell.GetStatus() )
+                
+        self.assertEqual( game.GetMines(), minesList )
+        self.assertEqual( len( game ) * len( game[ 0 ] ) - len( minesList ), game.GetToDiscover() )
+        
+if __name__ == '__main__':
+    unittest.main()
+    
