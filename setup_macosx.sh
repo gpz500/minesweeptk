0ed18e81e37dc2bd14ff0952e5e34e0bca511728
Alessandro Morgantini <gpz500@technologist.com>
2012-02-08 13:06:30 +0000

    Rivisti un pochino gli script per confezionare l'esportazione su Win32 e Mac OS X.

--- setup_macosx.sh
+++ setup_macosx.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+rm -rf build
+rm -rf dist
 python setup_macosx.py py2app
--- setup_win32.bat
+++ setup_win32.bat
@@ -1,3 +1,7 @@
+@echo off
+rmdir /s /q dist 2>nul
+rmdir /s /q build 2>nul
 python setup_win32.py py2exe
+if errorlevel 1 exit /b 1
 copy *.gif dist
 copy license.txt dist
